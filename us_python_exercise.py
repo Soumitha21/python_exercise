e2b6bd291ca43e71ef53101873198bdebd3ac55e
Soumitha21 <103850965+Soumitha21@users.noreply.github.com>
2025-01-01 20:28:41 +0530

    Add files via upload

---
+++ us_python_exercise.py
@@ -0,0 +1,14 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a=10
+print(a)
+10
+email="ilearniexcel@gmail.com"
+email[12]
+'@'
+email[13:18]
+'gmail'
+email[18:]
+'.com'
+email[0:12]
+'ilearniexcel'
